aeb9395f38863038c75481cf3b734b0d3ecda285
aimalexe <aimalexe@gmail.com>
2022-06-25 15:07:33 +0500

    main() func and edit function added

--- project.cpp
+++ project.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <stdio.h>
+#include <windows.h>
 using namespace std;
 
+void shoping ::edit(){
+	int productKey, token = 0, productNewCode;
+	string productName;
+	
+	cout<<"Modify The Record\n";
+	cout<<"Product code? ";
+	cin>>productKey;
+	
+	data.open("database.txt", ios::in);
+	if (!data){
+		cout<<"\nFile doesn't Found!!! ";
+		
+	}else{
+		Data.open("database1.txt", ios::app|ios::out);
+		data >> 
+	}
+}
+
 int main(){
-	//this is just a startup file.
-
-
-
+	shoping s;
+	s.menue();
 	return 0;
 }
