e8814b7e657f7fdd06c882bb2b54fae3b13db6aa
awaissaddiqui <awaissaddiqui143@gmail.com>
2022-06-25 02:01:27 -0700

    Code Pushed by Awais Saddiqui

--- project.cpp
+++ project.cpp
@@ -1,10 +1,74 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+main(){
+	class shopping
+	{
+		/* Declaring global variable for this class*/
+		private:                          
+			int productCode;
+			float price;
+			float discount;
+			string productName;
+			// Function declartion 
+		public:
+			void menu();
+			void administrator();
+			void buyer();
+			void add();
+			void edit();
+			void remove();
+			void list();
+			void receipt();
+			
+   };
+	void shopping :: menu()
+	{
+		m:
+		int choice ;
+		string email;
+		string password;
+		
+		cout<<"\n______Supermarket Main Menu______ \n\n";
+		cout<<"\n|  1) Administartor  |\n";
+		cout<<"\n|  2) Buyer  |\n";
+		cout<<"\n|  3) Exit  |\n";
+		cout<<"\n\n Please Select !";
+		cin>>choice;
+		
+		switch(choice)
+		{
+			case 1:
+			cout<<"Please Login \n";
+			cout<<"Enter Email ";
+			cin>>email;
+			cout<<"Enter Your Password";
+			cin>>password;
+			if(email=="uetp@gmail.com" && password== "uetp")
+			{
+				administrator();
+			}
+			else
+			{
+				cout<<"Invalid Email/Password";
+			}
+			break;
+		
+            case 2:
+	        {
+		   			buyer();
+			}
+			case 3:
+			{
+					exit();
+			}
+			default:{cout<<"Please Select from the given option"; }
+		}
+		
+goto m;
+	}
 
-int main(){
-	//this is just a startup file
 
 
 
-	return 0;
 }
